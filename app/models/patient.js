a810595f3200d83424cc75a22b61617d8692e087
fofombi <fofombi@gmail.com>
2019-09-02 07:23:34 -0400

    Today commit

--- app/models/patient.js
+++ app/models/patient.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 const patientSchema = new mongoose.Schema({
+  // MRN stands for Medical Record Number and it could take letters/Number
+  // Example of cases receive from Lemuel Shattick Hospital, MRN could be LS00011111222
   mrn: {
-    type: Number,
+    type: String,
     required: true
   },
 
@@ -22,12 +24,13 @@ const patientSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  // Here the Date is a string because this could allow the tech, to give more details on the collection Date and the service Date.
   collectionDate: {
-    type: Date,
+    type: String,
     required: true
   },
   serviceDate: {
-    type: Date,
+    type: String,
     required: true
   },
 
